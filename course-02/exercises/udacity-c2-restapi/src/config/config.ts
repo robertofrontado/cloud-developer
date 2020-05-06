24d3629a82a01c3c639d04bf0ec9c321e961b1df
Roberto Frontado <robertofrontado@gmail.com>
2020-05-06 14:51:37 +0200

    Added authentication & authorization

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,20 +1,11 @@
 export const config = {
-  "dev": {
-    "username": process.env.CD_DB_USERNAME,
-    "password": process.env.CD_DB_PASSWORD,
-    "database": process.env.CD_DB_DATABASE,
-    "host": process.env.CD_DB_HOST,
-    "dialect": 'postgres',
-    "aws_region": process.env.CD_AWS_REGION,
-    "aws_profile": process.env.CD_AWS_PROFILE,
-    "aws_media_bucket": process.env.CD_AWS_BUCKET,
-    "jwt_secret": process.env.CD_JWT_SECRET
-  },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
-  }
+  "username": process.env.CD_DB_USERNAME,
+  "password": process.env.CD_DB_PASSWORD,
+  "database": process.env.CD_DB_DATABASE,
+  "host": process.env.CD_DB_HOST,
+  "dialect": 'postgres',
+  "aws_region": process.env.CD_AWS_REGION,
+  "aws_profile": process.env.CD_AWS_PROFILE,
+  "aws_media_bucket": process.env.CD_AWS_BUCKET,
+  "jwt_secret": process.env.CD_JWT_SECRET
 }
